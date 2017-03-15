44d5476b310e5233a95fb873a750ea6049cd3f9a
Sam Snelling <sam@onroi.com>
2017-03-14 22:06:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,12 +30,13 @@ ufw allow 443
 ufw --force enable
 
 # Install base PHP packages
-apt-get install -y --force-yes php7.1-cli php7.1-dev \
-php7.1-pgsql php7.1-sqlite3 php7.1-gd \
-php7.1-curl php7.1-memcached \
-php7.1-imap php7.1-mysql php7.1-mbstring \
-php7.1-xml php7.1-zip php7.1-bcmath php7.1-soap \
-php7.1-intl php7.1-readline php7.1-mcrypt
+apt-get install -y --force-yes \
+    php7.1-cli php7.1-dev                           \
+    php7.1-pgsql php7.1-sqlite3 php7.1-gd           \
+    php7.1-curl php7.1-memcached                    \
+    php7.1-imap php7.1-mysql php7.1-mbstring        \
+    php7.1-xml php7.1-zip php7.1-bcmath php7.1-soap \
+    php7.1-intl php7.1-readline php7.1-mcrypt
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | php mv composer.phar /usr/local/bin/composer
